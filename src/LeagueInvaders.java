e5e8277ab5308fa8dc0bd4cb2e74b8e83ba36ddf
League Student <league@iMac-23.attlocal.net>
2020-01-26 11:25:36 -0800

    leagueInvaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,34 @@
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class LeagueInvaders {
-
+	
+	JFrame leagueInvadersJFrame;
+	GamePanel GP = new GamePanel();
+	
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	
+	LeagueInvaders(){
+		leagueInvadersJFrame = new JFrame();
+	}
+	
+	public static void main(String[] args) {
+		LeagueInvaders LI = new LeagueInvaders();
+		
+		LI.setup();
+	}
+	
+	void setup() {
+		leagueInvadersJFrame.add(GP);
+		leagueInvadersJFrame.setSize(WIDTH, HEIGHT);
+		leagueInvadersJFrame.setVisible(true);
+		
+		leagueInvadersJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		leagueInvadersJFrame.addKeyListener(GP);
+	}
+	
 }
